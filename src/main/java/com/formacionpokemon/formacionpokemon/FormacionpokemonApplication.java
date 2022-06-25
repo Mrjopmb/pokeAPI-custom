2bd3fb2f08623bfc483c383bbc28b1d7fcda64fb
Jorge Ivan Martinez Badillo <jopmb97@gmail.com>
2022-06-24 23:56:19 -0500

    Intento 2

--- src/main/java/com/formacionpokemon/formacionpokemon/FormacionpokemonApplication.java
+++ src/main/java/com/formacionpokemon/formacionpokemon/FormacionpokemonApplication.java
@@ -1,14 +1,99 @@
 package com.formacionpokemon.formacionpokemon;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionpokemon.formacionpokemon.controller.pokeService;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@RestController
 public class FormacionpokemonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FormacionpokemonApplication.class, args);
+	}
+	
+	@Autowired
+	private pokeService service;
+	
+	@GetMapping("data/{name}")
+	public String getAllData(@PathVariable String name) {
+		return service.getData(name);
+	}
+	
+	@GetMapping("abilities/{name}")
+	public List<Object> getPokeAbilities(@PathVariable String name) {
+		String res = service.getData(name);
+		JSONObject jsonObject = new JSONObject(res);
+		JSONArray array = jsonObject.optJSONArray("abilities");
+		for (int i = 0; i < array.length(); i++) {
+			JSONObject jsonO = array.getJSONObject(i);
+			JSONObject jsonI = jsonO.optJSONObject("ability");
+			String uri = jsonI.optString("url");
+			jsonI.put("information", new JSONObject(service.getFromURL(uri)));
+			jsonI.remove("url");
+			jsonO.remove("ability");
+			jsonO.put("ability", (Object)jsonI);
+		}
+		
+		return array.toList();
+	}
+	
+	@GetMapping("base-experience/{name}")
+	public String getPokeBaseExperience(@PathVariable String name) {
+		String res = service.getData(name);
+		JSONObject jsonObject = new JSONObject(res);
+		String str = "{\"base-experience\": "+ jsonObject.optInt("base_experience") +"}";
+		return str;
+	}
+	
+	@GetMapping("held-items/{name}")
+	public String getPokeHeldItems(@PathVariable String name) {
+		String res = service.getData(name);
+		JSONObject jsonObject = new JSONObject(res);
+		try {
+			String str = "{\"held-items\": "+ jsonObject.optJSONArray("held-items").toList() +"}";
+			return str;
+		} catch(Exception e) {
+			String str = "{\"held-items\": "+ new ArrayList<Object>() +"}";
+			return str;
+		}
+	}
+	
+	@GetMapping("id/{name}")
+	public String getPokeId(@PathVariable String name) {
+		String res = service.getData(name);
+		JSONObject jsonObject = new JSONObject(res);
+		String str = "{\"id\": "+ jsonObject.optInt("id") +"}";
+		return str;
+	}
+	
+	@GetMapping("name/{name}")
+	public String getPokeName(@PathVariable String name) {
+		String res = service.getData(name);
+		JSONObject jsonObject = new JSONObject(res);
+		JSONArray array = jsonObject.optJSONArray("forms");
+		String str = "{\"name\": "+ array.optJSONObject(0).optString("name") +"}";
+		return str;
+	}
+	
+	@GetMapping("location-area-encounters/{name}")
+	public String getPokeLocationAreaEncounters(@PathVariable String name) {
+		String res = service.getData(name);
+		JSONObject jsonObject = new JSONObject(res);
+		String uri = jsonObject.optString("location_area_encounters");
+		String resTwo = service.getFromURL(uri);
+		return resTwo;
 	}
 
 }
