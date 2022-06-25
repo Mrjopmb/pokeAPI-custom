205ce4caf54dd92d5a016a9667a41821c9a2bdcb
Jorge Ivan Martinez Badillo <78879996+Mrjopmb@users.noreply.github.com>
2022-06-24 23:49:24 -0500

    Update FormacionpokemonApplication.java

--- src/main/java/com/formacionpokemon/formacionpokemon/FormacionpokemonApplication.java
+++ src/main/java/com/formacionpokemon/formacionpokemon/FormacionpokemonApplication.java
@@ -2,7 +2,6 @@ package com.formacionpokemon.formacionpokemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FormacionpokemonApplication {
