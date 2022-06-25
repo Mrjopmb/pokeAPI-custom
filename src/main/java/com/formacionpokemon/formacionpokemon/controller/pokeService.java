fe0ae557f090319b2f1abe1044f694049dfe143a
Jorge Ivan Martinez Badillo <jopmb97@gmail.com>
2022-06-25 00:50:46 -0500

    backup

--- src/main/java/com/formacionpokemon/formacionpokemon/controller/pokeService.java
+++ src/main/java/com/formacionpokemon/formacionpokemon/controller/pokeService.java
@@ -1,13 +1,5 @@
 package com.formacionpokemon.formacionpokemon.controller;
 
-
-import java.util.Arrays;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,20 +10,13 @@ public class pokeService {
 	public String getData(String name) {
 		String uri = "https://pokeapi.co/api/v2/pokemon/" + name;
 		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-		//String result = restTemplate.getForObject(uri, String.class);
-		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
-		
-		return result.toString();
+		String result = restTemplate.getForObject(uri, String.class);
+		return result;
 	}
 	
 	public String getFromURL(String uri) {
 		RestTemplate restTemplate = new RestTemplate();
-		
 		String result = restTemplate.getForObject(uri, String.class);
-		
 		return result;
 	}
 
