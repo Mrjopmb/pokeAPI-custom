e116340001a342414f03e87ba05b3b13fec6997f
Jorge Ivan Martinez Badillo <jopmb@MacBook-Air-de-Jorge.local>
2022-07-07 17:31:59 -0500

    mas mas intentos

--- src/main/java/com/formacionpokemon/formacionpokemon/controller/pokeController.java
+++ src/main/java/com/formacionpokemon/formacionpokemon/controller/pokeController.java
@@ -25,17 +25,17 @@ public class pokeController {
 	
 	@GetMapping("/")
 	public String getAllData() {
-		System.out.println(service.testData());
-		//System.out.println(service.getData("zapdos"));
-		return "Hola Mundo!";
-		/*return "<B> PokeAPI API</B> <BR> <BR> "
+		//System.out.println(service.testData());
+		System.out.println(service.getData("zapdos"));
+		//return "Hola Mundo!";
+		return "<B> PokeAPI API</B> <BR> <BR> "
 				+ "1.- data/{pokemon} <BR>"
 				+ "2.-  abilities/{pokemon} <BR>"
 				+ "3.-  base-experience/{pokemon} <BR>"
 				+ "5.-  held-items/{pokemon} <BR>"
 				+ "6.-  id/{pokemon} <BR>"
 				+ "7.-  name/{pokemon} <BR>"
-				+ "8.-  location-area-encounters/{pokemon} <BR>";*/
+				+ "8.-  location-area-encounters/{pokemon} <BR>";
 	}
 	
 	@GetMapping("data/{name}")
